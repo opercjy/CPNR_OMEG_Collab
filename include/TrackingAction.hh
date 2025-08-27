3df0beae43274e03c3308ffa43da3751c6eecd58
Jiyoung CHOI <158591410+opercjy@users.noreply.github.com>
2025-08-28 01:06:05 +0900

    Update TrackingAction.hh

--- include/TrackingAction.hh
+++ include/TrackingAction.hh
@@ -5,16 +5,17 @@
 #include "globals.hh"
 
 //______________________________________________________________________________________
+// 입자 하나하나의 트랙(생성부터 소멸까지의 경로) 단위로 작업을 수행하는 클래스입니다.
 class TrackingAction : public G4UserTrackingAction
 {
 public:
   TrackingAction();
   virtual ~TrackingAction();
 
-  // 트랙이 시작되기 전에 호출됩니다.
+  // 트랙이 시작되기 직전에 호출됩니다.
   virtual void PreUserTrackingAction(const G4Track* track) override;
 
-  // 트랙이 끝난 후에 호출됩니다.
+  // 트랙이 끝난 직후에 호출됩니다.
   virtual void PostUserTrackingAction(const G4Track* track) override;
 };
 
