c910344b40c32ab24eff2da3ecfecccbb07ac6d4
Jiyoung CHOI <158591410+opercjy@users.noreply.github.com>
2025-08-28 04:17:54 +0900

    Update PhysicsList.cc

--- src/PhysicsList.cc
+++ src/PhysicsList.cc
@@ -1,34 +1,12 @@
 #include "PhysicsList.hh"
+#include "G4RadioactiveDecayPhysics.hh"
 
-// 필요한 물리 모델들의 헤더 파일을 포함합니다.
-#include "G4DecayPhysics.hh"             // 입자 붕괴
-#include "G4RadioactiveDecayPhysics.hh" // 방사성 붕괴 (핵심)
-#include "G4EmStandardPhysics_option4.hh" // 전자기 상호작용 (가장 정밀한 표준 모델)
-#include "G4HadronPhysicsFTFP_BERT.hh"  // 강입자 물리 (고에너지)
-#include "G4HadronElasticPhysics.hh"    // 강입자 탄성 산란
-#include "G4IonPhysics.hh"              // 이온 물리
-#include "G4StoppingPhysics.hh"         // 저에너지 하전 입자 물리
-
-//______________________________________________________________________________________
-// 생성자에서 필요한 물리 모델 객체를 생성하고 등록(RegisterPhysics)합니다.
-PhysicsList::PhysicsList() : G4VModularPhysicsList()
+// G4Shielding()이 아닌 Shielding() 생성자를 호출합니다.
+PhysicsList::PhysicsList() : Shielding()
 {
-  // 1. 입자 붕괴 물리
-  RegisterPhysics(new G4DecayPhysics());
-
-  // 2. 방사성 붕괴 물리 (60Co 붕괴 시뮬레이션을 위해 필수)
+  // Shielding 물리 리스트에 방사성 붕괴 모델을 추가로 등록합니다.
   RegisterPhysics(new G4RadioactiveDecayPhysics());
-
-  // 3. 전자기 물리 (광전효과, 컴프턴 산란, 쌍생성 등)
-  RegisterPhysics(new G4EmStandardPhysics_option4());
-
-  // 4. 강입자 물리 (FTFP_BERT 모델 기반)
-  RegisterPhysics(new G4HadronPhysicsFTFP_BERT());
-  RegisterPhysics(new G4HadronElasticPhysics());
-  RegisterPhysics(new G4IonPhysics());
-  RegisterPhysics(new G4StoppingPhysics());
 }
 
-//______________________________________________________________________________________
 PhysicsList::~PhysicsList()
 {}
