92ba76ee43dda247191c6f3205142d8fe593f4b1
Jiyoung CHOI <158591410+opercjy@users.noreply.github.com>
2025-08-27 20:45:08 +0900

    Update EventAction.hh

--- include/EventAction.hh
+++ include/EventAction.hh
@@ -15,7 +15,7 @@ public:
   virtual void BeginOfEventAction(const G4Event*) override;
 
   // 이벤트가 끝날 때 호출됩니다.
-  virtual void EndOfEventAction(const G-4Event*) override;
+  virtual void EndOfEventAction(const G4Event*) override;
 
   // SteppingAction에서 호출하여 에너지를 누적시키는 함수
   void AddEdep(G4double edep);
