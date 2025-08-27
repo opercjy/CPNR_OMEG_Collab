7ec4298ac75602f0259c8297a8166e12b83828d9
Jiyoung CHOI <158591410+opercjy@users.noreply.github.com>
2025-08-28 01:01:32 +0900

    Update PhysicsList.hh

--- include/PhysicsList.hh
+++ include/PhysicsList.hh
@@ -1,9 +1,11 @@
 #ifndef PhysicsList_h
 #define PhysicsList_h 1
 
+// Geant4의 모듈형 물리 리스트 기본 클래스를 상속받습니다.
 #include "G4VModularPhysicsList.hh"
 
 //______________________________________________________________________________________
+// 시뮬레이션에 사용될 물리 프로세스들을 정의하고 등록하는 클래스입니다.
 class PhysicsList : public G4VModularPhysicsList
 {
 public:
