4f1137e8d9a8feee467de80a68f54097a021d1d6
Jiyoung CHOI <158591410+opercjy@users.noreply.github.com>
2025-08-28 01:02:34 +0900

    Update PrimaryGeneratorAction.cc

--- src/PrimaryGeneratorAction.cc
+++ src/PrimaryGeneratorAction.cc
@@ -1,23 +1,29 @@
-#include "PrimaryGeneratorAction.hh"
+#ifndef PrimaryGeneratorAction_h
+#define PrimaryGeneratorAction_h 1
 
-#include "G4Event.hh"
-#include "G4GeneralParticleSource.hh"
+#include "G4VUserPrimaryGeneratorAction.hh"
+#include "globals.hh"
 
-//______________________________________________________________________________________
-PrimaryGeneratorAction::PrimaryGeneratorAction()
-: G4VUserPrimaryGeneratorAction(), fGPS(0)
-{
-  fGPS = new G4GeneralParticleSource();
-}
+// G4GeneralParticleSource 클래스를 사용하기 위한 전방 선언
+class G4GeneralParticleSource;
+class G4Event;
 
 //______________________________________________________________________________________
-PrimaryGeneratorAction::~PrimaryGeneratorAction()
+// 각 이벤트의 초기 입자를 생성하는 역할을 담당하는 클래스입니다.
+class PrimaryGeneratorAction : public G4VUserPrimaryGeneratorAction
 {
-  delete fGPS;
-}
+public:
+  PrimaryGeneratorAction();
+  virtual ~PrimaryGeneratorAction();
 
-//______________________________________________________________________________________
-void PrimaryGeneratorAction::GeneratePrimaries(G4Event* anEvent)
-{
-  fGPS->GeneratePrimaryVertex(anEvent);
-}
+  // Geant4 커널이 매 이벤트 시작 시 호출하는 함수입니다.
+  // 이 함수 안에서 초기 입자를 생성하고 이벤트에 추가합니다.
+  virtual void GeneratePrimaries(G4Event* anEvent) override;
+
+private:
+  // General Particle Source (GPS) 객체 포인터.
+  // GPS를 사용하면 매크로 파일에서 다양한 소스 설정을 쉽게 할 수 있습니다.
+  G4GeneralParticleSource* fGPS;
+};
+
+#endif
