f84ef83bf1a3ad4b91fb3d9dea8ed5b204f80191
Jiyoung CHOI <158591410+opercjy@users.noreply.github.com>
2025-08-28 04:17:33 +0900

    Update PhysicsList.hh

--- include/PhysicsList.hh
+++ include/PhysicsList.hh
@@ -1,12 +1,13 @@
 #ifndef PhysicsList_h
 #define PhysicsList_h 1
 
-// Geant4의 모듈형 물리 리스트 기본 클래스를 상속받습니다.
+// Shielding 클래스의 기반이 되는 헤더를 포함해주는 것이 안정적입니다.
 #include "G4VModularPhysicsList.hh"
+// 이제 경로를 지정해 줄 것이므로, 컴파일러가 이 파일을 찾을 수 있습니다.
+#include "Shielding.hh"
 
-//______________________________________________________________________________________
-// 시뮬레이션에 사용될 물리 프로세스들을 정의하고 등록하는 클래스입니다.
-class PhysicsList : public G4VModularPhysicsList
+// G4Shielding이 아닌 Shielding 클래스를 상속받습니다.
+class PhysicsList : public Shielding
 {
 public:
   PhysicsList();
