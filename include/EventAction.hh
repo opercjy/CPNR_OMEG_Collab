d16ec890ad776fc336f3d1622f271716286aceae
Jiyoung CHOI <158591410+opercjy@users.noreply.github.com>
2025-08-30 21:39:58 +0900

    Update EventAction.hh

--- include/EventAction.hh
+++ include/EventAction.hh
@@ -4,30 +4,22 @@
 #include "G4UserEventAction.hh"
 #include "globals.hh"
 
-class G4Event;
-
-//______________________________________________________________________________________
-// 각 이벤트의 시작과 끝에서 필요한 작업을 수행하는 클래스입니다.
-// 주로 이벤트별 데이터 초기화 및 저장을 담당합니다.
+/**
+ * @class EventAction
+ * @brief 각 이벤트(Event)의 시작과 끝에서 필요한 작업을 수행하는 클래스입니다.
+ *
+ * 이 프로젝트에서는 이벤트가 끝날 때마다 HitsCollection에 저장된 데이터를 읽어와,
+ * 이벤트 요약 정보(EventSummary TTree)와 상세 Hit 정보(Hits TTree)를
+ * G4AnalysisManager를 통해 ROOT 파일에 기록하는 핵심적인 역할을 합니다.
+ */
 class EventAction : public G4UserEventAction
 {
 public:
   EventAction();
   virtual ~EventAction();
 
-  // 이벤트가 시작되기 직전에 호출됩니다.
-  virtual void BeginOfEventAction(const G4Event*) override;
-
-  // 이벤트가 끝난 직후에 호출됩니다.
+  // 이벤트가 끝난 직후에 호출되어 데이터 저장 로직을 수행합니다.
   virtual void EndOfEventAction(const G4Event*) override;
-
-  // SteppingAction에서 호출하여 스텝별 에너지를 누적시키는 함수입니다.
-  void AddEdep(G4double edep);
-
-private:
-  // 해당 이벤트 동안 특정 볼륨(여기서는 LS)에 쌓인 총 에너지를 저장하는 변수입니다.
-  // BeginOfEventAction에서 0으로 초기화됩니다.
-  G4double fEdep;
 };
 
 #endif
