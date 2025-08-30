6504e87c873a640628c5225e23c298adaa306a82
Jiyoung CHOI <158591410+opercjy@users.noreply.github.com>
2025-08-30 22:18:39 +0900

    Update PrimaryGeneratorAction.cc

--- src/PrimaryGeneratorAction.cc
+++ src/PrimaryGeneratorAction.cc
@@ -1,17 +1,9 @@
-// PrimaryGeneratorAction.cc
-// PrimaryGeneratorAction 클래스의 멤버 함수들을 구현합니다.
-
 #include "PrimaryGeneratorAction.hh"
-
 #include "G4Event.hh"
 #include "G4GeneralParticleSource.hh" // GPS 헤더 파일 포함
 
-//______________________________________________________________________________________
 /**
- * @brief 생성자 (Constructor)
- *
- * PrimaryGeneratorAction 객체가 생성될 때 호출됩니다.
- * G4GeneralParticleSource(GPS) 객체를 생성하여 멤버 변수에 할당합니다.
+ * @brief 생성자: G4GeneralParticleSource(GPS) 객체를 생성합니다.
  */
 PrimaryGeneratorAction::PrimaryGeneratorAction()
 : G4VUserPrimaryGeneratorAction(), fGPS(nullptr)
@@ -19,26 +11,21 @@ PrimaryGeneratorAction::PrimaryGeneratorAction()
   fGPS = new G4GeneralParticleSource();
 }
 
-//______________________________________________________________________________________
 /**
- * @brief 소멸자 (Destructor)
- *
- * PrimaryGeneratorAction 객체가 소멸될 때 호출됩니다.
- * 생성했던 GPS 객체를 삭제하여 메모리 누수를 방지합니다.
+ * @brief 소멸자: 생성했던 GPS 객체를 삭제하여 메모리 누수를 방지합니다.
  */
 PrimaryGeneratorAction::~PrimaryGeneratorAction()
 {
   delete fGPS;
 }
 
-//______________________________________________________________________________________
 /**
- * @brief GeneratePrimaries
- * @param anEvent 현재 이벤트에 대한 정보를 담고 있는 객체 포인터
+ * @brief 매 이벤트 시작 시 Geant4 커널에 의해 호출됩니다.
+ * @param anEvent 현재 이벤트 객체
  *
- * 매 이벤트 시작 시 Geant4 커널에 의해 호출됩니다.
- * GPS가 매크로 파일(.mac)에 정의된 설정에 따라 입자를 생성하여
- * 현재 이벤트(anEvent)에 주입하는 역할을 합니다.
+ * GPS의 GeneratePrimaryVertex() 함수를 호출하여, 매크로 파일(.mac)에 정의된
+ * 설정에 따라 초기 입자를 생성하고 현재 이벤트에 주입합니다.
+ * 이 방식은 C++ 코드를 재컴파일하지 않고도 소스 설정을 자유롭게 변경할 수 있게 해줍니다.
  */
 void PrimaryGeneratorAction::GeneratePrimaries(G4Event* anEvent)
 {
